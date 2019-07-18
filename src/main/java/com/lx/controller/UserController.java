6feb617b7d5e2d733ec26c47944017b2418e3ec5
Renaaa <ren812848195@163.com>
2019-07-18 21:12:52 +0800

    提注册、注册验证手机号是否存在接口

--- src/main/java/com/lx/controller/UserController.java
+++ src/main/java/com/lx/controller/UserController.java
@@ -15,10 +15,43 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    /**
+     * 登录接口
+     * @param user
+     * @return
+     */
     @RequestMapping("/login")
     @ApiOperation(value = "登录接口")
     public String login(User user) {
         boolean result = userService.login(user);
+        if (result) {
+            return "success";
+        } else {
+            return "fail";
+        }
+    }
+
+    /**
+     * 注册接口
+     * @param user
+     * @return
+     */
+    @RequestMapping("/register")
+    @ApiOperation(value = "注册接口")
+    public String register(User user) {
+        userService.register(user);
+        return "index";
+    }
+
+    /**
+     * 注册时验证手机号是否已经存在
+     * @param phoneNumber
+     * @return
+     */
+    @RequestMapping("/validatePhoneNum")
+    @ApiOperation(value = "注册时验证手机号是否存在接口")
+    public String validatePhoneNum(String phoneNumber) {
+        boolean result = userService.validatePhoneNum(phoneNumber);
         if (result) {
             return "success";
         } else {
